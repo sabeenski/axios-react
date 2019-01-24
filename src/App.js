6ee55eaa4151396fd82f14efdb99ba81fc7f718c
sabeenski <sabinadhaugoda@gmail.com>
2019-01-24 17:21:34 +0100

    axios http request

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import axios from 'axios'
+
 import './App.css';
+import UserForm from './components/UserForm';
 
 class App extends Component {
+
+  state={
+    repos: null
+  }
+
+  getUser = (e) => {
+    e.preventDefault()
+    const user = e.target.username.value
+    if(!user) alert ("Please enter the user name")
+    else {(axios.get(`https://api.github.com/users/${user}`))
+    .then((res) => {
+      console.log(res)
+      const repos = res.data.public_repos
+      this.setState({repos})
+    })}
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="main">
+          <header className="App-header">
+            <h1 className="App-title">HTTP calls in React</h1> 
+          </header>
+          <div className="form-container">
+            <h5>Find the number of Github repos your friends have!</h5>
+            <UserForm getUser={this.getUser}/>
+            { this.state.repos && <p className="result">Number of repos: {this.state.repos}</p>}   
+          </div>
+        </div>
       </div>
     );
   }
